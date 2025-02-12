b1fec39613c809f3a6543fcc6bdfbe7fa05655ec
rishabh-mogi <rishabh.jaiswal@mogiio.com>
2025-02-12 12:37:10 +0530

    added changes in sdk

--- src/APIRedisCache.ts
+++ src/APIRedisCache.ts
@@ -1,32 +1,37 @@
-import Redis from "ioredis";
 import { RedisClient } from "./RedisClient";
 import { Request, Response, NextFunction } from "express";
 
-export class APIRedisCache extends RedisClient {
-    constructor(private service: string = "unknown", private environment: string = "dev") {
-        super();
-        if (!this.client) this.connect();
+export class APIRedisCache {
+    
+    private client;
+    constructor() {
+        this.client = RedisClient.getInstance().getClient();
     }
 
     public cacheMiddleware = async (req: Request, res: Response, next: NextFunction) => {
         const cacheKey = req.originalUrl;
-        console.log("cache Middleware get Called ",this.cacheMiddleware)
+        console.log("🔵 Cache middleware called for:", cacheKey);
+
         try {
             if (req.method === "GET") {
                 const cachedData = await this.client.get(cacheKey);
-                if (cachedData) return res.send(JSON.parse(cachedData));
+                if (cachedData) {
+                    console.log("⚡ Returning cached data for", cacheKey);
+                    return res.send(JSON.parse(cachedData));
+                }
             } else if (["POST", "PUT"].includes(req.method)) {
-                console.log("cache Middleware get Called ",res.locals.data)
                 res.on("finish", async () => {
                     if (res.statusCode === 200) {
+                        console.log("⚡ Caching response for", cacheKey);
                         await this.client.set(cacheKey, JSON.stringify(res.locals.data || {}), "EX", 3600);
                     }
                 });
             } else if (req.method === "DELETE") {
+                console.log("🚮 Deleting cache for", cacheKey);
                 await this.client.del(cacheKey);
             }
         } catch (error) {
-            console.error("Redis cache error:", error);
+            console.error("❌ Redis cache error:", error);
         }
 
         next();
--- src/index.ts
+++ src/index.ts
@@ -2,22 +2,6 @@ import { RedisClient } from "./RedisClient";
 import { APIRedisCache } from "./APIRedisCache";
 
 
-// function mainModule(){
-
-//     try {
-
-//         const redisClient = RedisClient.getInstance();
-//         redisClient.connect();
-
-//         const apiRedisCache = neRedisClientw APIRedisCache("test-service", "dev");
-//     }
-//     catch(err) {
-//         console.log("Facing Error while making instance of RedisClient and APIRedisCache");
-//     }
-
-// }
-
-
 module.exports = {
     RedisClient,
     APIRedisCache
